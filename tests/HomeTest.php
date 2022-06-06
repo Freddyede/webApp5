d112980d30c463f635f488f8caa2bc69fc802f78
Franck <patouillardfranck3@gmail.com>
2022-06-06 20:30:33 +0200

    edit test

--- tests/HomeTest.php
+++ tests/HomeTest.php
@@ -5,7 +5,7 @@ namespace App\Tests;
 use PHPUnit\Framework\TestCase;
 
 class HomeTest extends TestCase {
-    public function testSomething(): void {
+    public function testSomething() {
         $this->assertTrue(true);
     }
 }
