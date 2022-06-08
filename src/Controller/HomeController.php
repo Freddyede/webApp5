e12f70f700fa192580df65703a8d63bd6d3f2b37
Franck <patouillardfranck3@gmail.com>
2022-06-08 10:06:50 +0200

    Change road of HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="app_")
+ */
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
