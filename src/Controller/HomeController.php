d9f3aed9bb035d9e5432fcf15f801ae288a35450
Freddyede <f.patouillard@it-students.fr>
2022-07-04 20:59:04 +0200

    Update import

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
+    public function index(): Response {
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
